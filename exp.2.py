06d86215469f6c76009824e2eba0eef5c28855f5
rukhsar007cse <113462761+rukhsar007cse@users.noreply.github.com>
2023-10-16 23:54:05 +0530

    Add files via upload

---
+++ exp.2.py
@@ -0,0 +1,12 @@
+# Define a list
+my_list = [1, 2, 3, 4, 5]
+
+# Iterate over the list using a for loop
+for item in my_list:
+    print(item)
+# Define a dictionary
+my_dict = {'name': 'John', 'age': 30, 'city': 'New York'}
+
+# Iterate over the dictionary using a for loop
+for key, value in my_dict.items():
+    print(f'{key}: {value}')
